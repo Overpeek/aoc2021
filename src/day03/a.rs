a0d617685c7587401f1605be4217c85f58fe1f26
Overpeek <overpeek.fin@gmail.com>
2021-12-03 12:59:47 +0200

    day 3

--- src/day03/a.rs
+++ src/day03/a.rs
@@ -1,3 +1,23 @@
 pub fn main() {
-    println!("{} = {}", module_path!(), super::super::I);
+    let bitlen = super::DATA.lines().next().unwrap().len();
+    let nums: Vec<u16> = super::DATA
+        .lines()
+        .map(|line| u16::from_str_radix(line, 2).unwrap())
+        .collect();
+    let numc = nums.len();
+
+    let result = nums.into_iter().fold(vec![0; bitlen], |mut acc, num| {
+        acc.iter_mut()
+            .enumerate()
+            .for_each(|(i, acc)| *acc += ((1 << i) & num) >> i);
+        acc
+    });
+
+    let gamma = result.iter().enumerate().fold(0, |acc, (i, bitc)| {
+        acc + (((numc < 2 * *bitc as usize) as u32) << i)
+    });
+
+    let epsilon = gamma ^ (2_u32.pow(bitlen as u32) - 1);
+
+    println!("{} = {}", module_path!(), gamma * epsilon);
 }
--- src/day03/b.rs
+++ src/day03/b.rs
@@ -1,3 +1,39 @@
+// lhs = o2
+// rhs = co2
+fn run(nums: &[u16], bit_i: usize) -> (Vec<u16>, Vec<u16>) {
+    // lhs = 0 bit at bit_i
+    // rhs = 1 bit at bit_i
+    let (lhs, rhs) = nums
+        .iter()
+        .partition::<Vec<u16>, _>(|num| (1 << bit_i) & *num == 0);
+
+    match lhs.len().cmp(&rhs.len()) {
+        std::cmp::Ordering::Equal => (rhs, lhs),
+        std::cmp::Ordering::Less => (rhs, lhs),
+        std::cmp::Ordering::Greater => (lhs, rhs),
+    }
+}
+
 pub fn main() {
-    println!("{} = {}", module_path!(), super::super::I);
+    let bitlen = super::DATA.lines().next().unwrap().len();
+    let nums: Vec<u16> = super::DATA
+        .lines()
+        .map(|line| u16::from_str_radix(line, 2).unwrap())
+        .collect();
+
+    let (mut o2, mut co2) = run(&nums, bitlen - 1);
+    for i in (0..bitlen - 1).rev() {
+        if o2.len() != 1 {
+            o2 = run(&o2, i).0;
+        }
+        if co2.len() != 1 {
+            co2 = run(&co2, i).1;
+        }
+    }
+    let (o2, co2) = (
+        *o2.first().unwrap() as usize,
+        *co2.first().unwrap() as usize,
+    );
+
+    println!("{} = {}", module_path!(), o2 * co2);
 }
